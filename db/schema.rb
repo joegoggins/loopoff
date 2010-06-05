149504e80206f92c53e9edbe43f961c4ba85f921
Joe Goggins <joe.goggins@gmail.com>
2010-06-05 12:24:47 -0500

    added config.gem grit

--- db/schema.rb
+++ db/schema.rb
@@ -9,16 +9,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100603002617) do
+ActiveRecord::Schema.define(:version => 20100527025950) do
 
   create_table "directories", :force => true do |t|
     t.string   "slug"
-    t.string   "path"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "export_dirs", :force => true do |t|
     t.string   "path"
     t.datetime "created_at"
     t.datetime "updated_at"
