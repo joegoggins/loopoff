b9125663b85b1a1cd0417a574dbf3063867b3220
Joe Goggins <joe.goggins@gmail.com>
2010-06-07 00:40:45 -0500

    bitchin, now rendering in view

--- app/controllers/archive_loopoff_controller.rb
+++ app/controllers/archive_loopoff_controller.rb
@@ -1,11 +1,5 @@
 class ArchiveLoopoffController < Loader::DbController
   def show
-    @all_blobs = [] 
-    @db.repo.commits.each do |c|
-      c.tree.blobs.each do |b|
-        @all_blobs << b.id
-      end
-    end
-    @all_blobs.uniq!
+    @all_distinct_blobs_aggregated = @db.repo.all_distinct_blobs_aggregated
   end
 end
