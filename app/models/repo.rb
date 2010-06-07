1daf7f5c1606b2c7d013cbd79894209dcff582d1
Joe Goggins <joe.goggins@gmail.com>
2010-06-07 00:18:43 -0500

    all_distinct_blobs

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,2 +1,29 @@
-class Repo < Grit::Repo
+class Repo < Grit::Repo  
+  def all_distinct_blobs
+    distinct_blob_ids = []
+    returning [] do |t|
+      self.commits.each do |c|
+        c.tree.blobs.each do |b|
+          unless distinct_blob_ids.include?(b.id)            
+            t << b
+            distinct_blob_ids << b.id
+          end
+        end
+      end
+    end
+  end
+  
+  def all_distinct_blobs_aggregated
+    # returning [] do |t|
+    #   self.commits.each do |c|
+    #     tree_blobs = []
+    #     c.tree.blobs.each do |b|
+    #       tree_blobs << b
+    #     end
+    #     t << tree_blobs
+    #   end
+    #         
+    # end
+    # self.all_distinct_blobs.group_by {|x| x.name.split('_').first}.sort.delete_if {|x| x.first.match /\./}
+  end
 end
