8511a547b4ea925f59d5739b0fb80182ea6d21ca
Joe Goggins <joe.goggins@gmail.com>
2010-06-07 21:55:11 -0500

    UnarchivedPath.new_blobs and identical_blobs

--- app/models/db.rb
+++ app/models/db.rb
@@ -16,6 +16,19 @@ class Db < Dir
     self.all.first
   end
   
+  def self.[](db_name)
+    self.all.detect {|x| x.name == db_name.to_s}
+  end
+  
+  # A utility helper for filtering a set of file names
+  #
+  def self.loopoff_files(full_paths_to_files)
+    full_paths_to_files.select do |x|
+      # TODO: add x.match(/README/) when ready
+      (x.match(/.WAV/)) && File.file?(x)
+    end
+  end
+  
   def name
     File.basename(self.path)
   end
@@ -30,21 +43,26 @@ class Db < Dir
   end
   
   # any dir containing wav files that's not in /repo
-  def unarchived_paths
-    returning [] do |t|
-      Dir.chdir(self.path) do
-        Find.find('.') do |f|
-          if File.basename(f) == 'repo'
-            Find.prune 
-          elsif File.directory?(f)
-            if Dir.entries(f).any? {|e| e.match(/\.wav/i)}              
-              t << UnarchivedPath.new(self,File.expand_path(f))
-            else
-              next
-            end
+  def unarchived_paths(*args)
+    t = []
+    Dir.chdir(self.path) do
+      Find.find('.') do |f|
+        if File.basename(f) == 'repo'
+          Find.prune 
+        elsif File.directory?(f)
+          if Dir.entries(f).any? {|e| e.match(/\.wav/i)}              
+            t << UnarchivedPath.new(self,File.expand_path(f))
+          else
+            next
           end
         end
       end
     end
+    if args.first.kind_of? Regexp
+      return t.select {|x| x.name.match(args.first)}
+    elsif args.first.kind_of? String
+      return t.detect {|x| x.name == args.first}
+    end
+    return t
   end
 end
--- app/models/unarchived_path.rb
+++ app/models/unarchived_path.rb
@@ -1,5 +1,5 @@
 class UnarchivedPath < Dir
-  attr_reader :db
+  attr_reader :db, :repo
 
   def initialize(db, absolute_path)
     @db = db
@@ -13,4 +13,69 @@ class UnarchivedPath < Dir
   def to_param
     self.name
   end
+  
+  def new_blob_ids
+    self.distinct_blob_ids - self.db.repo.all_distinct_blobs.map(&:id)
+  end
+
+  def new_blobs
+    self.repo.tree.blobs.select {|x| self.new_blob_ids.include?(x.id)}
+  end
+  
+  def identical_blobs
+    self.repo.tree.blobs.select {|x| self.idential_blob_ids.include?(x.id)}
+  end
+  
+  def idential_blob_ids
+    self.distinct_blob_ids & self.db.repo.all_distinct_blobs.map(&:id)
+  end
+  
+  # makes a .git repo if not already created
+  def repo
+    begin
+      @repo ||= Grit::Repo.new(self.path)
+    rescue Grit::InvalidGitRepositoryError => e
+      raise "Sorry, gotta do this manually for now"
+      # c =`cd #{self.path} && git init`
+      # t = `#{c}`
+      # puts c
+      # puts t
+      # @repo = Grit::Repo.new(self.path)
+      #@repo = Grit::Repo.init_bare(File.join(self.path,'.git'))
+    end
+  end
+  
+  # WARNING DUPLICATION: from repo.rb
+  def distinct_blobs
+    if @distinct_blobs.blank?
+      distinct_blob_ids = []
+      @distinct_blobs = []
+      self.repo.commits.each do |c|
+        c.tree.blobs.each do |b|
+          unless distinct_blob_ids.include?(b.id)            
+            @distinct_blobs << b
+            distinct_blob_ids << b.id
+          end
+        end
+      end      
+    end
+    @distinct_blobs
+  end
+  
+  def distinct_blob_ids
+    self.distinct_blobs.map(&:id)
+  end
+  
+  def loopoff_files
+    Db.loopoff_files(self.entries.map {|x| File.join(self.path,x)})
+  end
+  
+  
+  # NOT WORKING: hell with this: no writes from ruby git, seems tricky/unstable
+  # def add_all
+  #     self.loopoff_files.each do |f|
+  #       self.repo.index.add(File.basename(f),File.read(f))  
+  #     end
+  #     self.repo.index.commit("adding all")
+  #   end
 end
