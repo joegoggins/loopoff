5940c312dd9ae988418a5d9d6a9ae31940b526db
Joe Goggins <joe.goggins@gmail.com>
2010-06-28 22:04:49 -0500

    Adding acts_as_taggable_on

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100614165542) do
+ActiveRecord::Schema.define(:version => 20100629025651) do
 
   create_table "directories", :force => true do |t|
     t.string   "slug"
@@ -45,5 +45,26 @@ ActiveRecord::Schema.define(:version => 20100614165542) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.string   "taggable_id",   :limit => 40
+    t.string   "taggable_type"
+    t.string   "tagger_id",     :limit => 40
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["context"], :name => "index_taggings_on_context"
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tagger_id", "tagger_type"], :name => "index_taggings_on_tagger_id_and_tagger_type"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  add_index "tags", ["name"], :name => "index_tags_on_name"
 
 end
